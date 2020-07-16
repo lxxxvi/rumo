995de86bc93ea1ec0e9c4d7cd6c53ee4ca159934
lxxxvi <lxxxvi@users.noreply.github.com>
2020-07-16 09:12:44 +0200

    test/controllers/home_controller_test.rb

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test 'get index'
-  test 'post index, invalid params'
-  test 'post index, valid params'
+  test 'get index' do
+    get '/'
+    assert_response :success
+  end
+
+  test 'post index, invalid params' do
+    post '/', params: { goto_hosts_form: { host_url_identifier: '' } }
+    assert_response :success
+
+    post '/', params: { goto_hosts_form: { host_url_identifier: 'does-not-exist' } }
+    assert_response :success
+  end
+
+  test 'post index, valid params' do
+    post '/', params: { goto_hosts_form: { host_url_identifier: 'cafe' } }
+    assert_redirected_to '/visit/cafe'
+    follow_redirect!
+    assert_response :success
+  end
 end
