3375f820333c1bbd0ecb313aabadb360d93180ab
lxxxvi <lxxxvi@users.noreply.github.com>
2020-07-16 14:04:02 +0200

    test/services/visit_statistics_service_test.rb

--- app/services/visits_statistics_service.rb
+++ app/services/visits_statistics_service.rb
@@ -1,15 +1,29 @@
 class VisitsStatisticsService
+  Statistics = Struct.new(:hash) do
+    def confirmed
+      hash[:confirmed] || 0
+    end
+
+    def unconfirmed
+      hash[:unconfirmed] || 0
+    end
+  end
+
   def initialize(host)
     @host = host
   end
 
   def statistics
-    @statistics ||= sql_result.reduce({}) do |result, item|
-      result.merge(Hash[item['status'], item['visit_count']])
-    end
+    @statistics ||= Statistics.new(sql_result_as_hash)
   end
 
   private
+
+  def sql_result_as_hash
+    sql_result.reduce({}) do |result, item|
+      result.merge(Hash[item['status'], item['visit_count']])
+    end.deep_symbolize_keys
+  end
 
   def sql_result
     @sql_result ||= Visit.connection.execute(statistics_sql)
