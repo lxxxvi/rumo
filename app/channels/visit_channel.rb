baae0f622108411c3b472ef91ef83d69f7ec5324
lxxxvi <lxxxvi@users.noreply.github.com>
2020-07-16 09:53:11 +0200

    test/channels/visit_channel_test.rb

--- app/channels/visit_channel.rb
+++ app/channels/visit_channel.rb
@@ -1,12 +1,14 @@
 class VisitChannel < ApplicationCable::Channel
   def subscribed
-    stream_for find_visit!
+    visit = find_visit
+    return reject if visit.nil?
+
+    stream_for visit
   end
 
   private
 
-  def find_visit!
-    Visit.of_uuid(current_uuid)
-         .find_by!(token: params[:token])
+  def find_visit
+    Visit.of_uuid(current_uuid).find_by(token: params[:token])
   end
 end
--- test/channels/visit_channel_test.rb
+++ test/channels/visit_channel_test.rb
@@ -1,8 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class VisitChannelTest < ActionCable::Channel::TestCase
   test 'subscribes' do
-    subscribe
+    visit = visits(:jim_cafe_20200101)
+    stub_connection current_uuid: visit.uuid
+
+    subscribe token: 'JIMCAFETOKEN'
     assert subscription.confirmed?
+
+    assert_has_stream_for visit
+  end
+
+  test 'not subscribes if wrong uuid/token' do
+    visit = visits(:jim_cafe_20200101)
+    stub_connection current_uuid: visit
+
+    subscribe token: 'PAMCAFETOKEN'
+    assert_no_streams
   end
 end
