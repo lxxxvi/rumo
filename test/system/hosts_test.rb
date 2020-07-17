28a5160e9ead10d695a5fcccc7505e2d0eeccb12
lxxxvi <lxxxvi@users.noreply.github.com>
2020-07-17 14:41:50 +0200

    wip, system/hosts_test.rb

--- test/system/hosts_test.rb
+++ test/system/hosts_test.rb
@@ -1,10 +1,41 @@
 require "application_system_test_case"
 
 class HostsTest < ApplicationSystemTestCase
-  test 'host signs up'
-  test 'host signs up, invalid params'
-  test 'host resets password'
-  test 'host resets password, invalid params'
-  test 'host signs in'
-  test 'host signs in, invalid params'
+  test 'host signs up' do
+    visit '/'
+
+    within 'section#host' do
+      click_on 'Sign up'
+    end
+
+    assert_selector 'h1', text: 'Sign up as host'
+
+    fill_in 'Name', with: "Moe's Tavern"
+    fill_in 'Email', with: 'moe@simp.son'
+    fill_in 'Password', with: 'abcdef'
+
+    click_on 'Sign up'
+
+    assert_selector 'h1', text: 'Admin area'
+  end
+
+  test 'host signs up, invalid params' do
+    assert false
+  end
+
+  test 'host resets password' do
+    assert false
+  end
+
+  test 'host resets password, invalid params' do
+    assert false
+  end
+
+  test 'host signs in' do
+    assert false
+  end
+
+  test 'host signs in, invalid params' do
+    assert false
+  end
 end
