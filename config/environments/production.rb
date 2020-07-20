3f7ef94eff2ab04c695d76fdeeaad35dc16ecd2a
lxxxvi <lxxxvi@users.noreply.github.com>
2020-07-20 18:59:42 +0200

    change protocol to https in default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
-  routes.default_url_options = { host: 'rumo-production.herokuapp.com' }
+  routes.default_url_options = {
+    host: 'rumo-production.herokuapp.com',
+    protocol: 'https'
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
