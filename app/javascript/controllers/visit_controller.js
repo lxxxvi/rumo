ecc930e544c40469f141770e6c6cdb709a1eee4b
lxxxvi <lxxxvi@users.noreply.github.com>
2020-10-26 16:14:37 +0100

    autofill visit form after first visit
    After first visit we store the "name" and "contact" field in the browser's localstorage.
    The second time the user opens the form, the fields will be autofilled.

--- app/javascript/controllers/visit_controller.js
+++ app/javascript/controllers/visit_controller.js
@@ -1,15 +1,31 @@
-// REMOVE ME?!
-// import { Controller } from "stimulus";
-// import RumoStorage from "../storages/rumo_storage";
+import { Controller } from "stimulus";
+import RumoStorage from "../storages/rumo_storage";
 
-// export default class extends Controller {
-//   static targets = ["forward"]
+export default class extends Controller {
+  static targets = ["contactField", "nameField"]
 
-//   connect() {
-//     this.rumoStorage = new RumoStorage(window);
-//     let token = this.data.get("token");
+  connect() {
+    this.rumoStorage = new RumoStorage(window);
 
-//     this.rumoStorage.mergeToken(token);
-//     window.location.href = this.forwardTarget.href;
-//   }
-// }
+    this.autofill(this.contactFieldTarget, this.rumoStorage.contact);
+    this.autofill(this.nameFieldTarget, this.rumoStorage.name);
+  }
+
+  autofill(target, value) {
+    target.value = target.value || value;
+  }
+
+  updateStorage() {
+    return new Promise((resolve, reject) => {
+      this.rumoStorage.contact = this.contactFieldTarget.value;
+      this.rumoStorage.name = this.nameFieldTarget.value;
+
+      resolve('OK');
+    });
+  }
+
+  update(event) {
+    event.preventDefault();
+    this.updateStorage().then((result) => { event.target.submit() });
+  }
+}
