37d050eacbb10ddebb3505fda5dd16aeabd53fb1
lxxxvi <lxxxvi@users.noreply.github.com>
2020-12-14 09:58:52 +0100

    update node packages, tailwind 2

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: [
+      './app/**/*.haml',
+      './app/**/*.css',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
