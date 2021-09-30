ef72e105a47746b7e59e06de3a07054e29aeefcc
mario <github@lxxxvi.ch>
2021-09-30 10:59:00 +0200

    update gems

--- test/services/qr_code_service_test.rb
+++ test/services/qr_code_service_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QrCodeServiceTest < ActiveSupport::TestCase
   test '#as_svg' do
     QrCodeService.new('https://foo.bar/baz').tap do |service|
-      assert_equal file_fixture('qr_codes/foo_bar_baz.svg').read, service.as_svg
+      assert_equal file_fixture('qr_codes/foo_bar_baz.svg').read.chomp, service.as_svg
     end
   end
 end
